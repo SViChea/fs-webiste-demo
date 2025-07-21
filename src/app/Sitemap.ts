68ca3c80d24866290ca1078fcfd6144f2ad8f599
ViChea <ssovicheasok@gmail.com>
2025-07-21 20:09:43 +0700

    Sitemap and robot.txt

---
+++ src/app/Sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap{
+    return [
+        {
+            url: 'https://sovicheasok.info',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1.0
+        },
+        {
+            url: 'https://www.sovicheasok.info/todos',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.9
+        },
+        {
+            url: 'https://www.sovicheasok.info/user',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8
+        },
+        {
+            url: 'https://www.sovicheasok.info/blog',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.7
+        },
+        {
+            url: 'https://www.sovicheasok.info/product',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.6
+        },
+        {
+            url: 'https://www.sovicheasok.info/about',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5
+        }
+    ]
+}
